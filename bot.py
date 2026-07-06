4e3395604b65622558f8cf963016189cf20f70cf
Lifted042 <agu08364@gmail.com>
2026-07-06 01:55:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -497,5 +497,5 @@ class NigerianAccountBot:
 target_url = "https://nnnrc.com/#/register"
 NUM_ACCOUNTS = 3
 
-bot = NigerianAccountBot(start_code=4869104)
+bot = NigerianAccountBot(start_code=4869116)
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
